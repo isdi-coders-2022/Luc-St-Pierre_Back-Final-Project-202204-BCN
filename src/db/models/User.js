86aea71b31fd86226306e9029c6e488aac30fa68
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-30 17:31:19 +0200

    Created User model

---
+++ src/db/models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+});
+
+const User = model("User", UserSchema, "users");
+
+module.exports = User;
