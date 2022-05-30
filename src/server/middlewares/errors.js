568df8b6a543bb8852b9bc7e69cca03818efde6d
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-30 20:10:04 +0200

    Created notFoundError middleware

---
+++ src/server/middlewares/errors.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const customError = require("../../utils/customError");
+
+const notFoundError = (req, res, next) => {
+  const error = customError("404", "Endpoint not found");
+
+  next(error);
+};
+
+module.exports = { notFoundError };
