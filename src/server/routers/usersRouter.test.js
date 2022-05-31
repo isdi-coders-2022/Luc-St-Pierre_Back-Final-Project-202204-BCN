3a89ee673b2a3e1b72711eb6b1d466e30298c0fa
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-31 14:05:52 +0200

    created supertest for new user

--- src/server/routers/usersRouter.test.js
+++ src/server/routers/usersRouter.test.js
@@ -3,7 +3,7 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 const { default: mongoose } = require("mongoose");
 const app = require("../index");
 const connectDB = require("../../db");
-const { usersMock } = require("../../mocks/usersMocks");
+const { usersMock, userMock } = require("../../mocks/usersMocks");
 const User = require("../../db/models/User");
 
 let database;
@@ -27,8 +27,15 @@ afterEach(async () => {
   await User.deleteMany();
 });
 
-describe("Given a POST /users/regiest endpoint", () => {
+describe("Given a POST /users/register endpoint", () => {
   describe("When it receives a request with non existing user", () => {
-    test("Then it should return with a response status code 201 with the new user created", () => {});
+    test("Then it should return with a response status code 201 with the new user created", async () => {
+      const { body } = await request(app)
+        .post("/users/register")
+        .send(userMock)
+        .expect(201);
+
+      expect(body).toHaveProperty("username", userMock.username);
+    });
   });
 });
