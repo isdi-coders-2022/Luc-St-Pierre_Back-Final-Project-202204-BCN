b03ddd72406ce1890711f468e6a229d95e22331d
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-31 12:31:59 +0200

    Created users mocks for testing

---
+++ src/mocks/usersMocks.js
@@ -0,0 +1,31 @@
+const userMockCredentials = {
+  username: "userX",
+  password: "passwordX",
+};
+
+const userMock = {
+  username: "userX",
+  password: "passwordX",
+  name: "userXMock",
+  email: "userXMock@gmail.com",
+  image: "image",
+};
+
+const usersMock = [
+  {
+    username: "userY",
+    password: "passwordY",
+    name: "userYMock",
+    email: "userYMock@gmail.com",
+    image: "image",
+  },
+  {
+    username: "userZ",
+    password: "passwordZ",
+    name: "userZMock",
+    email: "userZMock@gmail.com",
+    image: "image",
+  },
+];
+
+module.exports = { userMockCredentials, userMock, usersMock };
