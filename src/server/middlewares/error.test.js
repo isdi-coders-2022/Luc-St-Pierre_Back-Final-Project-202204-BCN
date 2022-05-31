f68849650ed4757fefaff625107f575fc12df702
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-31 10:05:24 +0200

    Created test for given a general error middleware

--- src/server/middlewares/error.test.js
+++ src/server/middlewares/error.test.js
@@ -1,4 +1,4 @@
-const { notFoundError } = require("./errors");
+const { notFoundError, generalError } = require("./errors");
 
 describe("Given a notFoundError middleware", () => {
   describe("When it's invoked with a response", () => {
@@ -8,6 +8,30 @@ describe("Given a notFoundError middleware", () => {
       notFoundError(null, null, next);
 
       expect(next).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a generalError middleware", () => {
+  describe("When it receives a response", () => {
+    test("Then it should call the response's method with a status code of 500", () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      const error = {
+        statusCode: 500,
+        message: "General error",
+      };
+
+      const expectedStatusCode = 500;
+      const expectedMessage = { error: true, message: "General error" };
+
+      generalError(error, null, res, null);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+      expect(res.json).toBeCalledWith(expectedMessage);
     });
   });
 });
