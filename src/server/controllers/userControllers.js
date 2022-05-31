d00c5e8fe8f77271a01328be9d38ebda09fde1dd
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-31 10:57:04 +0200

    Created encryptedPassword middlewarea nd userRegistration controller

---
+++ src/server/controllers/userControllers.js
@@ -0,0 +1,43 @@
+const debug = require("debug")("airbnb:server:controllers:user");
+const chalk = require("chalk");
+const bcrypt = require("bcrypt");
+
+const User = require("../../db/models/User");
+const customError = require("../../utils/customError");
+const encryptPassword = require("../../utils/encryptPassword");
+
+const userRegistration = async (req, res, next) => {
+  const { name, username, password, image } = req.body;
+
+  const user = await User.findOne({ username });
+
+  if (user) {
+    const error = customError(
+      409,
+      "bad request",
+      `Username ${username} already exists!`
+    );
+    next(error);
+  }
+
+  try {
+    const encryptedPassword = await encryptPassword(password);
+
+    const newUser = User.create({
+      name,
+      username,
+      password: encryptedPassword,
+      image,
+    });
+
+    debug(chalk.green(`user has been created with username: $${username}`));
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    error.code = 400;
+    error.message = "bad request";
+    next(error);
+  }
+};
+
+module.exports = userRegistration;
