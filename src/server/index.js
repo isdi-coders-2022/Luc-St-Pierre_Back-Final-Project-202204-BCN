7654188075262d7cbeb86e677c0a4f533f0d3747
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-31 11:06:59 +0200

    Added user route and middlwares erros in server index

--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,19 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 
+const usersRouter = require("./routers/usersRouter");
+const { notFoundError, generalError } = require("./middlewares/errors");
+
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+
+app.use("/users", usersRouter);
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = app;
