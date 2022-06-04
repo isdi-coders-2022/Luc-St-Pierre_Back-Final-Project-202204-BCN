bf0fab0538ed5022e6aeeb55f4c338041b67f979
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-04 10:31:44 +0200

    Added register and login middlewares validators to users route

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { userRegister, userLogin } = require("../controllers/userControllers");
 
+const loginValidation = require("../middlewares/loginValidation copy");
+const registerValidation = require("../middlewares/registerValidation");
+
 const router = express.Router();
 
-router.post("/register", userRegister);
-router.post("/login", userLogin);
+router.post("/register", registerValidation, userRegister);
+router.post("/login", loginValidation, userLogin);
 
 module.exports = router;
