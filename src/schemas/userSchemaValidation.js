adf0a30c0b75dbd0e2e0123bee85c39939893410
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-04 10:28:31 +0200

    Added message for schemas validation

--- src/schemas/userSchemaValidation.js
+++ src/schemas/userSchemaValidation.js
@@ -2,11 +2,28 @@ const { Joi } = require("express-validation");
 
 const registerValidationSchema = {
   body: Joi.object({
-    name: Joi.string().alphanum().min(3).max(15).trim(true).required(),
-    userName: Joi.string().alphanum().min(3).max(15).trim(true).required(),
-    email: Joi.string().email().trim(true).required(),
+    name: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(15)
+      .messages({ message: "name is required" })
+      .trim(true)
+      .required(),
+    userName: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(15)
+      .messages({ message: "username is required" })
+      .trim(true)
+      .required(),
+    email: Joi.string()
+      .email()
+      .messages({ message: "email is required" })
+      .trim(true)
+      .required(),
     password: Joi.string()
       .regex(/[a-zA-Z0-9]{3,30}/)
+      .messages({ message: "password is required" })
       .min(8)
       .trim(true)
       .required(),
@@ -15,9 +32,16 @@ const registerValidationSchema = {
 
 const loginValidationSchema = {
   body: Joi.object({
-    userName: Joi.string().alphanum().min(3).max(15).trim(true).required(),
+    userName: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(15)
+      .messages({ message: "username is required" })
+      .trim(true)
+      .required(),
     pasword: Joi.string()
       .regex(/[a-zA-Z0-9]{3,30}/)
+      .messages({ message: "password is required" })
       .min(8)
       .trim(true)
       .required(),
