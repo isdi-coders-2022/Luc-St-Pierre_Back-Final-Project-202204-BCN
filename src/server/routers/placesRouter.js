cf12d76dabd2104d90db87081dcd2f3af82e6fb9
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-08 00:06:08 +0200

    Added multer for user register image

--- src/server/routers/placesRouter.js
+++ src/server/routers/placesRouter.js
@@ -2,13 +2,13 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 
-const router = express.Router();
-
 const {
   createPlace,
   getAllPlaces,
 } = require("../controllers/placeControllers");
 const auth = require("../middlewares/auth");
+
+const router = express.Router();
 
 const upload = multer({
   dest: path.join("uploads", "images"),
--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const { validate } = require("express-validation");
+const multer = require("multer");
+const path = require("path");
+
 const {
   registerValidationSchema,
   loginValidationSchema,
@@ -9,7 +12,19 @@ const { userRegister, userLogin } = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router.post("/register", validate(registerValidationSchema), userRegister);
+const upload = multer({
+  dest: path.join("uploads", "images"),
+  limits: {
+    fileSize: 3000000,
+  },
+});
+
+router.post(
+  "/register",
+  upload.single("image"),
+  validate(registerValidationSchema),
+  userRegister
+);
 router.post("/login", validate(loginValidationSchema), userLogin);
 
 module.exports = router;
