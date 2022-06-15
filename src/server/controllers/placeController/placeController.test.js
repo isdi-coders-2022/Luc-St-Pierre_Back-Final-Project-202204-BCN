1029963e0dfc9aabdfc6b39088b9a5f44a01627e
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-15 20:53:05 +0200

    Added test for a request for a place with a valid token

---
+++ src/server/controllers/placeController/placeController.test.js
@@ -0,0 +1,30 @@
+const Place = require("../../../db/models/Place");
+const mockPlaces = require("../../../mocks/mockPlaces");
+const getPlaceById = require("./placeController");
+
+describe("Given a getPlaceById middleware", () => {
+  const req = {
+    params: {
+      placeId: "1",
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  describe("When invoked with a valid token and an existing place id in the request params", () => {
+    test("Then it should call the response's status method with 200 and the json method with the data of the place", async () => {
+      const expectedStatus = 200;
+      const expectedJsonResponse = { placeDetail: mockPlaces[0] };
+
+      Place.findById = jest.fn().mockResolvedValue(mockPlaces[0]);
+
+      await getPlaceById(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+      expect(res.json).toHaveBeenCalledWith(expectedJsonResponse);
+    });
+  });
+});
