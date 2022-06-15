de541c04756506e5bc62d0e8812b298b531060b0
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-15 23:26:31 +0200

    Added tests for imageUserConverter middleware

---
+++ src/server/middlewares/imageUserConverter.test.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const path = require("path");
+const imageUserConverter = require("./imageUserConverter");
+
+jest.mock("firebase/storage", () => ({
+  ref: jest.fn().mockReturnValue("ref"),
+  uploadBytes: jest.fn().mockResolvedValue({}),
+  getStorage: jest.fn(),
+  getDownloadURL: jest.fn().mockResolvedValue("url"),
+}));
+
+const next = jest.fn();
+
+describe("Given a imageUserConverter middleware", () => {
+  describe("When it receives a request with a file and the readFile fails", () => {
+    test("Then it should call the next received function", async () => {
+      const req = { file: { filename: "file" } };
+
+      jest.spyOn(path, "join").mockReturnValue(path.join("uploads", "images"));
+
+      jest
+        .spyOn(fs, "rename")
+        .mockImplementation((oldpath, newpath, callback) => {
+          callback();
+        });
+
+      jest.spyOn(fs, "readFile").mockImplementation((pathToRead, callback) => {
+        callback("readFileError");
+      });
+
+      await imageUserConverter(req, null, next);
+
+      expect(next).toHaveBeenCalledWith("readFileError");
+    });
+  });
+
+  describe("When it receives a request with a file and the rename fails", () => {
+    test("Then it should call the next received function", async () => {
+      const req = { file: { filename: "file" } };
+
+      jest.spyOn(path, "join").mockReturnValue(path.join("uploads", "images"));
+
+      jest
+        .spyOn(fs, "rename")
+        .mockImplementation((oldpath, newpath, callback) => {
+          callback("renameError");
+        });
+
+      await imageUserConverter(req, null, next);
+
+      expect(next).toHaveBeenCalledWith("renameError");
+    });
+  });
+
+  describe("When it receives a request with no file", () => {
+    test("Then it should call the next received function", async () => {
+      const req = { file: null };
+
+      await imageUserConverter(req, null, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
