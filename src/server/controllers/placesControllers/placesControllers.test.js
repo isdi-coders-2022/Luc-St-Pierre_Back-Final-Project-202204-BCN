4cf473fc366dfcf2f06d20f2454497c1078051f2
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-15 23:06:35 +0200

    Added test for error if invalid data when creating a new place

--- src/server/controllers/placesControllers/placesControllers.test.js
+++ src/server/controllers/placesControllers/placesControllers.test.js
@@ -1,9 +1,12 @@
-const { getAllPlaces } = require("./placesControllers");
+const path = require("path");
+
+const { getAllPlaces, createPlace } = require("./placesControllers");
 
 const mockPlaces = require("../../../mocks/mockPlaces");
 const Place = require("../../../db/models/Place");
+const User = require("../../../db/models/User");
 
-describe("Given a createPlace middleware", () => {
+describe("Given a getAllPlaces middleware", () => {
   const req = {
     username: "LearningX",
   };
@@ -13,7 +16,7 @@ describe("Given a createPlace middleware", () => {
     json: jest.fn(),
   };
 
-  describe("When it's invoked with a valide token", () => {
+  describe("When it's invoked with a valid token", () => {
     test("Then it should call the response's status method with status code 200 and json method with a list of places", async () => {
       const expectedStatus = 200;
       Place.find = jest.fn().mockResolvedValue(mockPlaces);
@@ -33,6 +36,42 @@ describe("Given a createPlace middleware", () => {
       await getAllPlaces(req, res, next);
 
       expect(next).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a createPlace controller", () => {
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  jest
+    .spyOn(path, "join")
+    .mockResolvedValueOnce("image")
+    .mockReturnValueOnce(true)
+    .mockResolvedValue(new Error());
+
+  describe("When invoked with a new place with invalid data in the request", () => {
+    test("Then it should call the next received function with an error 'Error while trying to create a place'", async () => {
+      const req = {
+        body: mockPlaces[0],
+        file: {
+          filename: "12794217722",
+          originalname: "image.jpg",
+        },
+      };
+
+      const expectedErrorMessage = "Error while trying to create a place";
+      const next = jest.fn();
+
+      const expectedError = new Error(expectedErrorMessage);
+
+      Place.create = jest.fn().mockRejectedValue();
+
+      await createPlace(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
     });
   });
 });
